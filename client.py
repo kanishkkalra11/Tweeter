1c1fd1d540c468c3ce4f5a24bd887de4e3b8b784
mohitmina <34353107+mohitmina@users.noreply.github.com>
2020-11-17 22:48:18 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 import socket
 import time
 
-host_ip = "127.0.0.1"
+buffer_size = [1024,2048,4096,8192] 
+i=1
 localport   = 12345
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host_ip,localport))
+s.connect((s.gethostname() ,localport))
 
 print("Connected \n")
 
@@ -14,9 +15,9 @@ s.send(bytes("DONE","utf-8"))
 check = s.recv(16)
 checkFlag = check.decode("utf-8")
 if checkFlag == "Yes":
-    data = s.recv(1024)
+    data = s.recv(buffer_size[i])
     while data:
-        data = s.recv(1024)
+        data = s.recv(buffer_size[i])
         print(data)
         input_ = input()
         s.send(input_.encode())
