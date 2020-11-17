73b61cd6604ab173dde6e2c703d02e049eb1462c
kavita-v <54227800+kavita-v@users.noreply.github.com>
2020-11-17 23:34:57 +0530

    basic login/register util

--- utils.py
+++ utils.py
@@ -1,5 +1,56 @@
 #libs
+BUFF = 1024 #buffer size
+
+def register_new_user(username, password):
+    #TODO add new user to database
+    return
 
 def authenticate(client_socket):
     #TODO login/register here
-    return
+    while True:
+        client_socket.send(
+            """
+            Please authenticate:
+            1: Login
+            2: Register
+            """.encode()
+        )
+        response = client_socket.recv(BUFF).decode()
+
+        if (response=='1'):
+            #login
+            client_socket.send("Username: ".encode())
+            username = client_socket.recv(BUFF).decode()
+            client_socket.send("Password: ".encode())
+            password = client_socket.recv(BUFF).decode()
+            
+            if (1): #TODO check username exists and password matches
+                client_socket.send("Login successful. Welcome back.")
+                break
+            else:
+                client_socket.send("Invalid username or password.".encode())
+                continue
+
+        elif (response=='2'):
+            #register
+            while True:
+                client_socket.send("Enter new username: ".encode())
+                username = client_socket.recv(BUFF).decode()
+                if (1): #TODO check if username taken already
+                    client_socket.send("Username not available".encode())
+                    continue
+                client_socket.send("Enter new password: ".encode())
+                password = client_socket.recv(BUFF).decode()
+                client_socket.send("Confirm new password: ".encode())
+                password_confirm = client_socket.recv(BUFF).decode()
+                if (password != password_confirm):
+                    client_socket.send("Password does not match".encode())
+                else:
+                    break
+            register_new_user(username, password) #TODO write register_new_user function in utils
+            break
+
+        else:
+            client_socket.send("Please select a valid option.")
+
+    return username
