941a9dac5ff1b52cf4666af0b6c292771728f477
kavita-v <54227800+kavita-v@users.noreply.github.com>
2020-11-18 23:25:44 +0530

    made separate file for database queries

---
+++ dataqueries.py
@@ -0,0 +1,25 @@
+BUFF = 1024 #buffer size
+
+def register_new_user(username, password):
+    #TODO add new user to database
+    return
+
+def check_user_pass(username, password):
+    #TODO check username exists and password matches
+    # return true if all correct
+    return
+
+def check_user_exists(username):
+    #TODO check if username taken already
+    # return true if username taken
+    return
+
+def get_trending_hashtags():
+    #TODO get top 5 trending hashtags as a list
+    hashtags = []
+    return hashtags
+
+def get_tweets_by_hashtag(hashtag):
+    #TODO return a list of max 10 recent tweets with input hashtag
+    tweets = []
+    return tweets
