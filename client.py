1cbfa61fbb4d89da08aaebf19aa326b829dc5048
mohitmina <34353107+mohitmina@users.noreply.github.com>
2020-11-18 21:21:20 +0530

    Update client.py

--- client.py
+++ client.py
@@ -5,29 +5,29 @@ buffer_size = [1024,2048,4096,8192]
 i=1
 localport   = 12345
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((s.gethostname() ,localport))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((client_socket.gethostname() ,localport))
 
 print("Connected \n")
 
-s.send(bytes("DONE","utf-8"))
+client_socket.send(bytes("DONE","utf-8"))
 
-check = s.recv(16)
+check = client_socket.recv(16)
 checkFlag = check.decode("utf-8")
 if checkFlag == "Yes":
-    data = s.recv(buffer_size[i])
+    data = client_socket.recv(buffer_size[i])
     while data:
-        data = s.recv(buffer_size[i])
+        data = client_socket.recv(buffer_size[i])
         print(data)
         input_ = input()
-        s.send(input_.encode())
+        client_socket.send(input_.encode())
         print("Your response has been sent")
 else:
     print("Error")
 
-s.shutdown(2)
+client_socket.shutdown(2)
 print("Bbyee")
-s.close()
+client_socket.close()
 
 
 
